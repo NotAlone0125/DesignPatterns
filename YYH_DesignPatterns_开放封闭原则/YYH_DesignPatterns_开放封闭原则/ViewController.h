a08a46fa64d5eac0ff2f54a589d4565a36d890f2
yyh_ios_code <542395875@qq.com>
2017-09-13 14:31:46 +0800

    开放-封闭原则

---
+++ YYH_DesignPatterns_开放封闭原则/YYH_DesignPatterns_开放封闭原则/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYH_DesignPatterns_开放封闭原则
+//
+//  Created by 杨昱航 on 2017/7/10.
+//  Copyright © 2017年 杨昱航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
